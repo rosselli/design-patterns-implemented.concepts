041c12caa9a22f8527869c72860f0039bc5c192f
Flavio Rosselli <rosselli00@gmail.com>
2019-06-05 18:18:21 -0500

    Some fixes in Memento.

--- patterns/behavioral/memento/domnikl/State.php
+++ patterns/behavioral/memento/domnikl/State.php
@@ -29,3 +29,4 @@ class State {
         return $this->state;
     }
 }
+
--- tests/domnikl/MementoTest.php
+++ tests/domnikl/MementoTest.php
@@ -10,19 +10,17 @@ class MementoTest extends TestCase {
     public function testOpenTicketAssignAndSetBackToOpen() {
         $ticket = new Ticket();
         $ticket->open();
-
         $openedState = $ticket->getState();
-        $this->assertSame(State::STATE_OPENED, (string) $ticket->getState());
 
+		$this->assertSame(State::STATE_OPENED, (string) $ticket->getState());
         $memento = $ticket->saveToMemento();
 
-        // assign the ticket
+        # assign
         $ticket->assign();
         $this->assertSame(State::STATE_ASSIGNED, (string) $ticket->getState());
 
         // now restore to the opened state, but verify that the state object has been cloned for the memento
         $ticket->restoreFromMemento($memento);
-
         $this->assertSame(State::STATE_OPENED, (string) $ticket->getState());
         $this->assertNotSame($openedState, $ticket->getState());
     }
